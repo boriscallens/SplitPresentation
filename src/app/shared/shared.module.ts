7346643e6e65884dc0e95add75f81e1c9052bda9
Boris Callens <boris.callens+github@gmail.com>
2019-09-29 14:11:51 +0200

    removed models from shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Ticket } from './models/ticket.model';
-import { Capture } from './models/capture.model';
-import { Bill } from './models/bill.model';
 
 @NgModule({
   imports: [
     CommonModule
-  ],
-  declarations: [
-    Capture,
-    Ticket,
-    Bill
-  ],
-  exports: [
-    Capture,
-    Ticket,
-    Bill
   ]
 })
 export class SharedModule { }
